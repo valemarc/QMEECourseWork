1f6b6a1451ab4ce959d8348f04d4bebb8cef8e50
valemarc <valentina.marconi@ioz.ac.uk>
2018-10-10 14:34:26 +0100

    Added file

--- Week2/Code/cfexercises2.py
+++ Week2/Code/cfexercises2.py
@@ -1,12 +1,33 @@
-def foo(x):
+#!/usr/bin/env python3
+
+"""Some functions exemplifying the use of control statements"""
+#docstrings are considered part of the running code (normal comments are
+#stripped). Hence, you can access your docstrings at run time.
+
+#__appname__ = 'Boilerplate python script'
+__author__ = 'Valentina Marconi (valentina.marconi@ioz.ac.uk)'
+__version__ = '0.0.1'
+#__license__ = "License for this code/program"
+
+## imports ##
+import sys # module to interface our program with the operating system
+
+## constants ##
+
+
+## functions ##
+def elevate(x):
+    """Elevate a number to the power of 0.5."""
     return x ** 0.5
 
-def foo2(x,y):
+def greatest(x,y):
+    """Return the greater number between the two."""
     if x>y:
         return x
     return y
 
-def foo3(x,y,z):
+def swap(x,y,z):
+    """Order numbers."""
     if x > y:
         tmp = y
         y = x
@@ -17,15 +38,29 @@ def foo3(x,y,z):
         y = tmp
     return [x,y,z]
 
-def foo4(x):
+def multiply(x):
+    """Multiply 1 by the number you provide plus 1."""
     result = 1
     for i in range(1, x + 1):
         result = result * i
     return result
 
-def foo5(x): # a recursive function
+def recurs(x): # a recursive function
+    """Recursive multiplication."""
     if x == 1:
         return 1
-    return x * foo5(x - 1)
+    return x * recurs(x - 1)
 
-foo5(10)
+def main(argv): #this can be modified to do anything you need
+    """ Main entry point of the program """
+    print(elevate(22))
+    print(greatest(33,35))
+    print(swap(120,130,140))
+    print(multiply(60))
+    print(recurs(59))
+    return 0
+
+if __name__ == "__main__": #this is what the interpreter looks for first
+    """Makes sure the "main" function is called from command line"""  
+    status = main(sys.argv) #catch all arguments fed into the def
+    sys.exit(status) #pass the arguments to the main function (above)
