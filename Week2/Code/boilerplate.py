09cef2bec36c1d495231f5dc2242de36d58cbe80
valemarc <valentina.marconi@ioz.ac.uk>
2018-10-10 11:52:46 +0100

    Submission

--- Week2/Code/boilerplate.py
+++ Week2/Code/boilerplate.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-"""Description of this program or application.
-    You can use several lines"""
+"""Boilerplate python script"""
 
-__appname__ = 'Boilerplate python'
+__appname__ = 'Boilerplate python script'
 __author__ = 'Valentina Marconi (valentina.marconi@ioz.ac.uk)'
 __version__ = '0.0.1'
 __license__ = "License for this code/program"
@@ -15,12 +14,12 @@ import sys # module to interface our program with the operating system
 
 
 ## functions ##
-def main(argv):
+def main(argv): #this can be modified to do anything you need
     """ Main entry point of the program """
     print('This is a boilerplate') # NOTE: indented using two tabs or 4 spaces
     return 0
 
-if __name__ == "__main__": 
+if __name__ == "__main__": #this is what the interpreter looks for first
     """Makes sure the "main" function is called from command line"""  
-    status = main(sys.argv)
-    sys.exit(status)
+    status = main(sys.argv) #catch all arguments fed into the def
+    sys.exit(status) #pass the arguments to the main function (above)
